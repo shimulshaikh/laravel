fe0ab4a04ee8eb8142c48bd31cd97816183ed754
shimulshaikh <shimulshaikhcse@gmail.com>
2020-10-24 08:55:25 +0600

    productCategorySeeder

--- database/seeds/productCategorySeeder.php
+++ database/seeds/productCategorySeeder.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 
-class ProductCategoryTableSeeder extends Seeder
+class productCategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,14 +14,13 @@ class ProductCategoryTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        		
-    	foreach (range(1, 5) as $index) {
 
-    		DB::table('product_catagories')->insert([
+        foreach (range(1, 5) as $index)
+        {
+        	DB::table('product_catagories')->insert([
 	            'brandName' => $faker->company,
 	            'slug' => $faker->name
         	]);
-
-    	}
+        }
     }
 }
